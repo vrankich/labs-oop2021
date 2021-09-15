2dec82f855c9caf3ffad724f9d9cc72752a57c21
vrankich <rankich@list.tu>
2021-09-15 15:34:39 +0300

    Add files

--- lab1/main.cpp
+++ lab1/main.cpp
@@ -5,11 +5,7 @@ int main()
 	sm::Matrix *matrix = sm::get_matrix();
 	sm::output("Matrix from input:\n", matrix);
 
-<<<<<<< HEAD
 	sm::change_matrix(matrix);
-=======
-	sm::make_new_matrix(matrix);
->>>>>>> 88be4e22a3c77e9bb9a116d43616eab7564e081b
 	sm::output("New matrix with sorted row:\n", matrix);
 
 	sm::delete_matrix(matrix);
